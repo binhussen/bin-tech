1b8e2b6388cb68208852420e1c90cd1aebec5dae
binhussen <mhdhsna@gmail.com>
2022-07-27 15:40:23 +0300

    Make Program daynamic

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navItem = document.querySelectorAll(".nav-item");
 const select = document.querySelector(".select");
+const more = document.querySelector(".btn-more");
+const cards = document.querySelector(".cards");
 
 const openMenu = () => {
 	navMenu.classList.remove("desktop");
@@ -31,4 +33,52 @@ document.addEventListener("keydown", (e) => {
 	if (e.key === "Escape") {
 		closeMenu();
 	}
+});
+
+const technologies = [
+	{
+		title: "Web Development",
+		image: "./assets/icon/icon-web.png",
+		content:
+			"Bin Tech is a nationwide innovation inclusive environment designed to ensure students.",
+	},
+	{
+		title: "Mobile App",
+		image: "./assets/icon/icon-mob.png",
+		content:
+			"Bin Tech is a nationwide innovation inclusive environment designed to ensure students.",
+	},
+	{
+		title: "Graphics Design",
+		image: "./assets/icon/icon-graphics.png",
+		content:
+			"Bin Tech is a nationwide innovation inclusive environment designed to ensure students.",
+	},
+	{
+		title: "Robot Dev",
+		image: "./assets/icon/icon-robot.png",
+		content:
+			"Bin Tech is a nationwide innovation inclusive environment designed to ensure students.",
+	},
+	{
+		title: "Network Design",
+		image: "./assets/icon/icon-network.png",
+		content:
+			"Bin Tech is a nationwide innovation inclusive environment designed to ensure students.",
+	},
+];
+
+const showCards = (i) => {
+	cards.innerHTML += `<article class="card">
+						<img class="card-img" src=${technologies[i].image} alt="" />
+						<h3 class="card-title">${technologies[i].title}</h3>
+						<p class="card-content">${technologies[i].content}
+						</p>
+					</article>`;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	technologies.forEach((pro, i) => {
+		showCards(i);
+	});
 });
