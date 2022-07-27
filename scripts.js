bd9ba47018d869fb264526042268ad4dba49b829
binhussen <mhdhsna@gmail.com>
2022-07-27 14:48:02 +0300

    Add hamburger menu function

---
+++ scripts.js
@@ -0,0 +1,33 @@
+/** @format */
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navItem = document.querySelectorAll(".nav-item");
+
+const openMenu = () => {
+	navMenu.classList.remove("desktop");
+	navMenu.classList.toggle("active");
+	hamburger.classList.toggle("active");
+};
+
+const closeMenu = () => {
+	navMenu.classList.add("desktop");
+	navMenu.classList.remove("active");
+	hamburger.classList.remove("active");
+};
+
+hamburger.addEventListener("click", () => {
+	openMenu();
+});
+
+navItem.forEach((n) => {
+	n.addEventListener("click", () => {
+		closeMenu();
+	});
+});
+
+document.addEventListener("keydown", (e) => {
+	if (e.key === "Escape") {
+		closeMenu();
+	}
+});
